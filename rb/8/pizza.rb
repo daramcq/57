1748b9e6661e04995990be24887cd84c6d49ce84
Dara McHugh <1451455+daramcq@users.noreply.github.com>
2019-09-15 21:58:23 +0100

    Add the rest of CLI cruft

--- rb/8/pizza.rb
+++ rb/8/pizza.rb
@@ -10,3 +10,17 @@ class PizzaSlicer
     total_slices/@people
   end
 end
+
+if __FILE__ == $0
+
+  print "How many pizzas? "
+  pizzas = gets.strip.to_i
+  print "How many people? "
+  people = gets.strip.to_i
+  print "How many slices per pizza? "
+  slices = gets.strip.to_i
+
+  slicer = PizzaSlicer.new(pizzas, people, slices)
+  slices_per_person = slicer.serve()
+  print "Each person gets #{slices_per_person} slices\n"
+end
